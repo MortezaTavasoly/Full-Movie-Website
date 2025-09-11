9e0f68091f4d0e2d4770ebbf6091a13f8d210cee
MortezaTavasoly <vector.1475225963@gmail.com>
2025-09-11 23:56:12 +0330

    feat: adding types

--- frontend/src/pages/home/carousel/types.ts
+++ frontend/src/pages/home/carousel/types.ts
@@ -2,9 +2,12 @@ export interface Movie {
   id: number;
   title: string;
   imageUrl: string;
+  rating: number;
+  releaseDate: number;
+  genres: string[];
 }
 
 export interface CarouselProps {
   movies: Movie[];
-  autoPlayInterval?: number; // میلی‌ثانیه
+  autoPlayInterval?: number;
 }
